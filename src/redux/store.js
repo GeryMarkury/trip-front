2aef43cdd78ebe8a556ce8e50c56fe66af98b1f9
Heorhii <hmarkov.dev@gmail.com>
2024-02-22 13:26:59 +0100

    edit persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,4 @@
-import {
-	configureStore,
-	createSerializableStateInvariantMiddleware,
-	// Tuple,
-} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { tripsReducer } from "./tripsSlice.js";
 import { filterReducer } from "./filterSlice";
 import { authReducer } from "./auth/slice.js";
@@ -24,19 +20,16 @@ const authPersistConfig = {
 	whitelist: ["token"],
 };
 
-const ignoredActions = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
-
-const serializableMiddleware = createSerializableStateInvariantMiddleware({
-	ignoredActions,
-});
-
 export const store = configureStore({
 	reducer: {
 		auth: persistReducer(authPersistConfig, authReducer),
 		trips: tripsReducer,
 		filter: filterReducer,
 	},
-	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(serializableMiddleware),
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: { ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] },
+		}),
 	devTools: process.env.NODE_ENV === "development",
 });
 
